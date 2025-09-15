27cf470ce08ad2e8d157a86298c6e41c0b0c2266
Uniqueyou <wangyixuan@selectdb.com>
2025-09-15 10:19:55 +0800

    Fix some cases (#4444)

--- regression-test/suites/cloud/recycler/test_recycler_with_drop_mv.groovy
+++ regression-test/suites/cloud/recycler/test_recycler_with_drop_mv.groovy
@@ -76,7 +76,7 @@ suite("test_recycler_with_drop_mv") {
     HashSet<String> tabletIdSet3 = tabletIdSet2.stream().filter(tabletId -> !tabletIdSet1.contains(tabletId)).collect(Collectors.toSet());
     logger.info("tabletIdSet3:${tabletIdSet3}")
     assertTrue(tabletIdSet3.size() > 0)
-    rowCount = sql "select count(*) from ${tableName}"
+    def rowCount = sql "select count(*) from ${tableName}"
     logger.info("rowCount:{}", rowCount)
     assertEquals(rowCount[0][0], 150000)
 
