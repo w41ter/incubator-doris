3f547f66c82fcf8724715bf41f9395aa6c1c9503
meiyi <meiyi@selectdb.com>
2025-09-30 16:29:50 +0800

    [feat](snapshot) ms support update_snapshot and recycle call abort_multipart_upload (#4888)

--- cloud/src/enterprise/snapshot/snapshot_manager.h
+++ cloud/src/enterprise/snapshot/snapshot_manager.h
@@ -19,6 +19,9 @@ public:
     void begin_snapshot(std::string_view instance_id,
                         const doris::cloud::BeginSnapshotRequest& request,
                         doris::cloud::BeginSnapshotResponse* response) override;
+    void update_snapshot(std::string_view instance_id,
+                         const doris::cloud::UpdateSnapshotRequest& request,
+                         doris::cloud::UpdateSnapshotResponse* response) override;
     void commit_snapshot(std::string_view instance_id,
                          const doris::cloud::CommitSnapshotRequest& request,
                          doris::cloud::CommitSnapshotResponse* response) override;
