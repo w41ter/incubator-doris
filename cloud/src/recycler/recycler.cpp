7bcb27957b927830f06b501187b3ca6aac2eacea
Gavin Chou <gavin@selectdb.com>
2025-12-15 12:27:55 +0800

    [Fix](recycler) Fix possible null pointer when first txn->get() failed #59020 (#6203)
    fix first txn->get ends up with null pointer
    partial pick https://github.com/apache/doris/pull/59019

--- cloud/src/recycler/recycler.cpp
+++ cloud/src/recycler/recycler.cpp
@@ -2648,7 +2648,7 @@ int InstanceRecycler::scan_and_recycle(
     };
 
     std::unique_ptr<RangeGetIterator> it;
-    do {
+    while (it == nullptr /* may be not init */ || (it->more() && !stopped())) {
         if (get_range_retried > 1000) {
             err = "txn_get exceeds max retry, may not scan all keys";
             ret = -1;
@@ -2687,7 +2687,7 @@ int InstanceRecycler::scan_and_recycle(
             err = "loop_done error";
             ret = -1;
         }
-    } while (it->more() && !stopped());
+    }
     return ret;
 }
 
